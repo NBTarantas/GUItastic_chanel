44f83152e0c65e881065d613a8425bc1beada869
NBTarantas <150782839+NBTarantas@users.noreply.github.com>
2023-11-14 16:15:12 +0200

    Delete GUItastic_site directory
    Signed-off-by: NBTarantas <150782839+NBTarantas@users.noreply.github.com>

--- GUItastic_site/script.js
+++
@@ -1,9 +0,0 @@
-function sayHi() {
-    var confDialog = window.confirm("Хочете поділитись посиланням на цю сторінку?")
-  if (confDialog)
-   window.alert("Чудово!")
-  else
-   window.alert("Погано :(")
-}
-
-setTimeout(sayHi, 10099);
